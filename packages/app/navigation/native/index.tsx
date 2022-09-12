0d5ae594e98c5edfe4de345a051de957b9ad27bc
fer-oshiro <fernanda.sayuri@outlook.com>
2022-09-12 20:20:43 -0300

    initial commit

---
+++ packages/app/navigation/native/index.tsx
@@ -0,0 +1,48 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import { HomeScreen } from '../../features/home/screen'
+import { UserDetailScreen } from '../../features/user/detail-screen'
+
+const Stack = createNativeStackNavigator<{
+  home: undefined
+  'user-detail': {
+    id: string
+  }
+}>()
+
+const Tab = createBottomTabNavigator();
+
+export function App() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="home"
+        component={HomeScreen}
+        options={{
+          title: 'Home',
+        }}
+      />
+      <Stack.Screen
+        name="user-detail"
+        component={UserDetailScreen}
+        options={{
+          title: 'User',
+        }}
+      />
+    </Stack.Navigator>
+    
+  );
+}
+
+export function NativeNavigation() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Calendar" component={HomeScreen} />
+      <Tab.Screen name="PunchIn" component={UserDetailScreen} />
+      <Tab.Screen name="Stats" component={UserDetailScreen} />
+      <Tab.Screen name="Setting" component={UserDetailScreen} />
+    </Tab.Navigator>
+  )
+}
---
+++ readme.md
@@ -0,0 +1,37 @@
+# Punch In
+
+
+## Tecnologia utilizadas
+👾 [Solito](https://example.solito.dev) - template para navegação cross-platform 
+[Icon](https://www.svgrepo.com/collection/miscellaneous) - Ícones Principais
+- `moti` for animations
+- `dripsy` for theming/design (you can bring your own, too)
+- Expo SDK 44
+- Next.js 12
+- React Navigation 6
+
+## 🗂 Folder layout
+
+- `apps` entry points for each app
+
+  - `expo`
+  - `next`
+
+- `packages` shared packages across apps
+  - `app` you'll be importing most files from `app/`
+    - `features` (don't use a `screens` folder. organize by feature.)
+    - `provider` (all the providers that wrap the app, and some no-ops for Web.)
+    - `navigation` Next.js has a `pages/` folder. React Native doesn't. This folder contains navigation-related code for RN. You may use it for any navigation code, such as custom links.
+
+You can add other folders inside of `packages/` if you know what you're doing and have a good reason to.
+
+## 🏁 Start the app
+
+- Install dependencies: `yarn`
+
+- Next.js local dev: `yarn web`
+  - Runs `yarn next`
+- Expo local dev: `yarn native`
+  - Runs `expo start`
+
+
